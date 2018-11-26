5a9feb6503e3684da5fc341583e3d58ecfd896fa
albotw <yann.trou@gmail.com>
2018-11-26 16:13:10 +0100

    amelioration du sit'e
    je sais plus

--- JS/connexion.js
+++ JS/connexion.js
@@ -1,26 +1,26 @@
 var login = sessionStorage.login;
 var password = sessionStorage.password;
-
+document.getElementById("compte").style.display = "none";
 document.getElementById("submit").onclick = function(event){
     console.log("debut de la conexion");
     event.preventDefault();
 
     if (document.getElementById("login").value == login && document.getElementById("password").value == password){
         document.getElementById("connect_form").style.display = "none";
-        document.getElementById("output").innerHTML += "nom: "+sessionStorage.nom+"<br/>";
-        document.getElementById("output").innerHTML += "prénom: "+sessionStorage.prenom+"<br/>";
-        document.getElementById("output").innerHTML += "sexe: "+sessionStorage.sexe+"<br/>";
-        document.getElementById("output").innerHTML += "jour de naissance: "+sessionStorage.jour+"<br/>";
-        document.getElementById("output").innerHTML += "mois de naissance: "+sessionStorage.mois+"<br/>";
-        document.getElementById("output").innerHTML += "année de naissance :"+sessionStorage.annee+"<br/>";
-        document.getElementById("output").innerHTML += "ville:"+sessionStorage.ville+"<br/>";
-        document.getElementById("output").innerHTML += "code postal: "+sessionStorage.code_postal+"<br/>";
-        document.getElementById("output").innerHTML += "adresse:"+sessionStorage.adresse+"<br/>";
-        document.getElementById("output").innerHTML += "Académie:"+sessionStorage.academie+"<br/>";
-        document.getElementById("output").innerHTML += "Filière: "+sessionStorage.filiere+"<br/>";
-        document.getElementById("output").innerHTML += "Structure: "+sessionStorage.structure+"<br/>";
-        document.getElementById("output").innerHTML += "Orientation sexuelle: "+sessionStorage.orientation_sexuelle+"<br/>";
-        document.getElementById("output").innerHTML += "<br/><div id='flush_account'>supprimer le compte dans la session locale</div>";
+        document.getElementById("compte").style.display = "";
+        document.getElementById("nom").innerHTML = sessionStorage.nom;
+        document.getElementById("prenom").innerHTML = sessionStorage.prenom;
+        document.getElementById("sexe").innerHTML = sessionStorage.sexe;
+        document.getElementById("jour").innerHTML = sessionStorage.jour;
+        document.getElementById("mois").innerHTML = sessionStorage.mois;
+        document.getElementById("annee").innerHTML = sessionStorage.annee;
+        document.getElementById("ville").innerHTML = sessionStorage.ville;
+        document.getElementById("code_postal").innerHTML = sessionStorage.code_postal;
+        document.getElementById("addresse").innerHTML = sessionStorage.addresse;
+        document.getElementById("academie").innerHTML = sessionStorage.academie;
+        document.getElementById("filiere").innerHTML = sessionStorage.filiere;
+        document.getElementById("structure").innerHTML = sessionStorage.structure;
+        document.getElementById("orientation_sexuelle").innerHTML = sessionStorage.orientation_sexuelle;
     }
     else{
         document.getElementById("output").innerHTML="Impossible de se connecter: mauvais mot de passe ou login";
